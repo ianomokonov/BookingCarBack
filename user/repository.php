bc6a2029f7e3cfb71356b718d83c75c0a4fc9113
Nomokonov Ivan <nomokonov.vana@gmail.com>
2020-03-21 12:52:59 +0300

    BC-5: вход и регистрация без токена

--- user/repository.php
+++ user/repository.php
@@ -3,24 +3,63 @@
     include_once '../utils/database.php';
     include_once 'model.php';
     class UserRepository{
-        private $db;
+        private $database;
         private $base_table = 'users';
 
         public function __construct()
         {
-            $this->db = new DataBase();
+            $this->database = new DataBase();
         }
 
         public function SignIn(AddUser $user = null){
-            return "SignIn";
+            if($user){
+                try{
+                    unset($user->id);
+                    $user->password = password_hash($user->password, PASSWORD_BCRYPT);
+                    $this->db->genInsertQuery($user, $this->base_table);
+                    return new UserResponse("token", $this->GetUserById($this->database->db->lastInsertId()));
+                } catch(Exception $e) {
+                    return array("message" => "Ошибка добавления пользователя", "error" => $e);
+                }
+                
+            } else {
+                http_response_code(500);
+                return array("message" => "Пользователь не может быть пустым");
+            }
         }
 
         public function LogIn(LoginUser $user = null){
-            return "LogIn";
+            if($user){
+                try{
+                    $sth = $this->database->db->prepare("SELECT * FROM ".$this->base_table." WHERE email = ? AND password = ? LIMIT 1");
+                    $sth->setFetchMode(PDO::FETCH_CLASS, 'User');
+                    $sth->execute(array($user->email, password_hash($user->password, PASSWORD_BCRYPT)));
+                    $fullUser = $sth->fetch();
+                    return new UserResponse("token", $this->GetUserById($fullUser->id));
+                } catch(Exception $e) {
+                    return array("message" => "Ошибка входа пользователя", "error" => $e);
+                }
+            } else {
+                http_response_code(500);
+                return array("message" => "Введите данные для входа");
+            }
         }
 
         public function GetUser(string $token){
             return "GetUser";
+        }
+
+        private function GetUserById(int $id){
+            if($id){
+                $sth = $this->database->db->prepare("SELECT * FROM ".$this->base_table." WHERE id = ?");
+                $sth->setFetchMode(PDO::FETCH_CLASS, 'User');
+                $sth->execute(array($id));
+                return $sth->fetch();
+            } else {
+                http_response_code(500);
+                return array("message" => "GetUserById -> id не может быть пустым");
+            }
+            
         }
 
     }
